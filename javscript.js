8387b3bf54887cf7bfd774b161b7e92a7b1f7f96
NotADev900 <haoleeuk03@gmail.com>
2024-09-16 16:46:29 +0800

    Changed color accent

--- javscript.js
+++ javscript.js
@@ -1,11 +1,3 @@
-function whenClicked(nav) {
-  const navSelector = document.querySelector(`.${nav}`);
-  if (navSelector) {
-    navSelector.classList.add("nav_underline");
-    console.log("Success");
-  }
-}
-
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
