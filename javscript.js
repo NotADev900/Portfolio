97e4e11bae612023cefe4e48a8fe2c4ba9f28de3
NotADev900 <haoleeuk03@gmail.com>
2024-09-17 18:29:25 +0800

    Update V1.2
    NavBar changes on smaller screen sizes

--- javscript.js
+++ javscript.js
@@ -1,3 +1,19 @@
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 };
+
+const menuIcon = document.querySelector("#menu-icon");
+const navBar = document.querySelector(".navbar");
+const navBarLinks = document.querySelectorAll(".navbar a");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navBar.classList.toggle("active");
+};
+
+navBarLinks.forEach((link) => {
+  link.onclick = () => {
+    navBar.classList.toggle("active");
+    menuIcon.classList.toggle("bx-x");
+  };
+});
